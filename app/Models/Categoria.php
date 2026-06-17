8a7000ab52de70e8570577aea9bb5916bc600fdd
herman23gon <herman.gcp@gmail.com>
2026-06-17 08:15:22 -0400

    Modelo, migracion y controlador de Categoria creados

---
+++ app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'estado',
+    ];
+
+    public function trajes()
+    {
+        return $this->hasMany(Traje::class);
+    }
+}
